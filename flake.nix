0548fceef3d0bfd20feb065113e688b76d2d224c
vrad <vrad+notarealemail@vradford.com>
2026-04-08 22:04:30 -0400

    Add project files, version locks

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "game";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }: let
+    inherit (nixpkgs) lib;
+    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          sdl3
+          uv
+        ];
+      };
+    });
+  };
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# game
+
+## vincent's todos
+
+- fix tutorial communication
+- parameterize difficulty
+  - beep block galaxy style warnings
+- see about sprites
